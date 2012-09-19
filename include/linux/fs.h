a1dc19113713966d20da0825449bc74cdef2326d
faux123 <reioux@gmail.com>
2012-09-18 23:23:13 -0700

    fs/dyn_sync_cntrl: dynamic sync control
    The dynamic sync control interface uses Android kernel's unique early
    suspend / lat resume interface.
    
    While screen is on, file sync is disabled
    when screen is off, a file sync is called to flush all outstanding writes
    and restore file sync operation as normal.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2079,6 +2079,7 @@ static inline int thaw_bdev(struct block_device *bdev, struct super_block *sb)
 }
 #endif
 extern int sync_filesystem(struct super_block *);
+extern void sync_filesystems(int wait);
 extern const struct file_operations def_blk_fops;
 extern const struct file_operations def_chr_fops;
 extern const struct file_operations bad_sock_fops;

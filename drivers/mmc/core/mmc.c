34976aa5e5ce2dd20c899171f9f4af8a1d7a4498
Sujit Reddy Thumma <sthumma@codeaurora.org>
2012-10-11 17:17:03 +0530

    mmc: core: Add load based clock scaling support
    The SD3.0/eMMC4.5/SDIO3.0 cards can support clock rates upto
    200MHz (SDR104 or HS200 bus speed modes). For some workloads
    like video playback it isn't necessary for these cards to run
    at such high speed. Running at lower frequency, say 50MHz, in
    such cases can still meet the deadlines for data transfers.
    Scaling down the clock frequency dynamically has huge power
    savings not only because the bus is running at lower frequency
    but also has an advantage of scaling down the system core
    voltage, if supported.
    
    Provide an ondemand clock scaling support similar to cpufreq
    ondemand governor having two thresholds, up_threshold and
    down_threshold to decide whether to increase the frequency or
    scale it down respectively. The sampling interval is in the
    order of milliseconds and should be chosen by host drivers that
    enable MMC_CAP2_CLK_SCALE capability to take advantage of clock
    scaling. The sampling interval mainly depends on the the clock
    switching delays and hence a host driver decision. If sampling
    interval is too low frequent switching of frequencies can lead
    to high power consumption and if sampling interval is too high,
    the clock scaling logic would take long time to realize that the
    underlying hardware (controller and card) is busy and scale up
    the clocks.
    
    Change-Id: I22a5054beec41b0b66b3bf030ddfcf284de448b3
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/mmc/core/core.c
    	include/linux/mmc/host.h

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1436,6 +1436,7 @@ static void mmc_remove(struct mmc_host *host)
 
 	mmc_claim_host(host);
 	host->card = NULL;
+	mmc_exit_clk_scaling(host);
 	mmc_release_host(host);
 }
 
@@ -1486,6 +1487,12 @@ static int mmc_suspend(struct mmc_host *host)
 	BUG_ON(!host);
 	BUG_ON(!host->card);
 
+	/*
+	 * Disable clock scaling before suspend and enable it after resume so
+	 * as to avoid clock scaling decisions kicking in during this window.
+	 */
+	mmc_disable_clk_scaling(host);
+
 	mmc_claim_host(host);
 	if (mmc_can_poweroff_notify(host->card) &&
 		(host->caps2 & MMC_CAP2_POWER_OFF_VCCQ_DURING_SUSPEND)) {
@@ -1529,6 +1536,13 @@ static int mmc_resume(struct mmc_host *host)
 	err = mmc_init_card(host, host->ocr, host->card);
 	mmc_release_host(host);
 
+	/*
+	 * We have done full initialization of the card,
+	 * reset the clk scale stats and current frequency.
+	 */
+	if (mmc_can_scale_clk(host))
+		mmc_init_clk_scaling(host);
+
 	return err;
 }
 
@@ -1536,11 +1550,17 @@ static int mmc_power_restore(struct mmc_host *host)
 {
 	int ret;
 
+	/* Disable clk scaling to avoid switching frequencies intermittently */
+	mmc_disable_clk_scaling(host);
+
 	host->card->state &= ~(MMC_STATE_HIGHSPEED | MMC_STATE_HIGHSPEED_200);
 	mmc_card_clr_sleep(host->card);
 	mmc_claim_host(host);
 	ret = mmc_init_card(host, host->ocr, host->card);
 	mmc_release_host(host);
+
+	if (mmc_can_scale_clk(host))
+		mmc_init_clk_scaling(host);
 
 	return ret;
 }
@@ -1677,6 +1697,10 @@ int mmc_attach_mmc(struct mmc_host *host)
 	mmc_claim_host(host);
 	if (err)
 		goto remove_card;
+
+	/* Initialize clock scaling only for high frequency modes */
+	if (mmc_card_hs200(host->card) || mmc_card_ddr_mode(host->card))
+		mmc_init_clk_scaling(host);
 
 	return 0;
 

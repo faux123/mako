a3201aa154e58855ba08b50d8b4e7569da458c26
faux123 <reioux@gmail.com>
2012-11-22 10:33:09 -0800

    Overclocking: limit initial max to system default
    Signed-off-by: faux123 <reioux@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -241,7 +241,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 		cur_freq = table[index].frequency;
 	}
 
-	policy->cur = cur_freq;
+	policy->cur = CONFIG_MSM_CPU_FREQ_MAX;
 
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;

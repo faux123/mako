9a6abad9af994131f82b8e2f86aa99cbf074fad8
faux123 <reioux@gmail.com>
2012-12-10 08:55:33 -0800

    gamma control: fix wip code derps :p
    Signed-off-by: faux123 <reioux@gmail.com>

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -242,9 +242,15 @@ static ssize_t kgamma_r_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
+	kgamma[0] = 0xd0;
 	for (i=0; i<10; i++) {
-		pr_info("kgamma_r [%d] => %d \n", i, kgamma[i]);
+		pr_info("kgamma_r_p [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[5].payload[i] = kgamma[i];
+	}
+
+	kgamma[0] = 0xd1;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_r_n [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[6].payload[i] = kgamma[i];
 	}
 
@@ -277,9 +283,15 @@ static ssize_t kgamma_g_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
+	kgamma[0] = 0xd2;
 	for (i=0; i<10; i++) {
-		pr_info("kgamma_g [%d] => %d \n", i, kgamma[i]);
+		pr_info("kgamma_g_p [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[7].payload[i] = kgamma[i];
+	}
+
+	kgamma[0] = 0xd3;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_g_n [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[8].payload[i] = kgamma[i];
 	}
 
@@ -312,9 +324,15 @@ static ssize_t kgamma_b_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
+	kgamma[0] = 0xd4;
 	for (i=0; i<10; i++) {
-		pr_info("kgamma_b [%d] => %d \n", i, kgamma[i]);
+		pr_info("kgamma_b_p [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[9].payload[i] = kgamma[i];
+	}
+
+	kgamma[0] = 0xd5;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_b_n [%d] => %d \n", i, kgamma[i]);
 		faux123_power_on_set_1[10].payload[i] = kgamma[i];
 	}
 

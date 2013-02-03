de466398261ad03396a4391fed66b00eb0e75f5c
faux123 <reioux@gmail.com>
2013-02-02 16:39:15 -0800

    dio: Convert direct_IO to use iov_iter
    Change the direct_IO aop to take an iov_iter argument rather than an iovec.
    This will get passed down through most filesystems so that only the
    __blockdev_direct_IO helper need be aware of whether user or kernel memory
    is being passed to the function.
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    heavily backported for Mako from LKML reference
    
    Signed-off-by: faux123 <reioux@gmail.com>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -181,6 +181,7 @@ blkdev_get_block(struct inode *inode, sector_t iblock,
 	return 0;
 }
 
+#if 0
 static int
 blkdev_get_blocks(struct inode *inode, sector_t iblock,
 		struct buffer_head *bh, int create)
@@ -208,16 +209,17 @@ blkdev_get_blocks(struct inode *inode, sector_t iblock,
 		set_buffer_mapped(bh);
 	return 0;
 }
+#endif
 
 static ssize_t
-blkdev_direct_IO(int rw, struct kiocb *iocb, const struct iovec *iov,
-			loff_t offset, unsigned long nr_segs)
+blkdev_direct_IO(int rw, struct kiocb *iocb, struct iov_iter *iter,
+			loff_t offset)
 {
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file->f_mapping->host;
 
-	return __blockdev_direct_IO(rw, iocb, inode, I_BDEV(inode), iov, offset,
-				    nr_segs, blkdev_get_blocks, NULL, NULL, 0);
+	return __blockdev_direct_IO(rw, iocb, inode, I_BDEV(inode), iter,
+				    offset, blkdev_get_block, NULL, NULL, 0);
 }
 
 int __sync_blockdev(struct block_device *bdev, int wait)

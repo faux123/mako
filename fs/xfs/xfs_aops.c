ac3ee56697fa6d48e3791939167f7794ad290a20
faux123 <reioux@gmail.com>
2013-02-02 16:39:15 -0800

    dio: Convert direct_IO to use iov_iter
    Change the direct_IO aop to take an iov_iter argument rather than an iovec.
    This will get passed down through most filesystems so that only the
    __blockdev_direct_IO helper need be aware of whether user or kernel memory
    is being passed to the function.
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    heavily backported for Mako from LKML reference
    
    Signed-off-by: faux123 <reioux@gmail.com>

--- fs/xfs/xfs_aops.c
+++ fs/xfs/xfs_aops.c
@@ -1334,9 +1334,8 @@ STATIC ssize_t
 xfs_vm_direct_IO(
 	int			rw,
 	struct kiocb		*iocb,
-	const struct iovec	*iov,
-	loff_t			offset,
-	unsigned long		nr_segs)
+	struct iov_iter		*iter,
+	loff_t			offset)
 {
 	struct inode		*inode = iocb->ki_filp->f_mapping->host;
 	struct block_device	*bdev = xfs_find_bdev_for_inode(inode);
@@ -1344,7 +1343,7 @@ xfs_vm_direct_IO(
 	ssize_t			ret;
 
 	if (rw & WRITE) {
-		size_t size = iov_length(iov, nr_segs);
+		size_t size = iov_iter_count(iter);
 
 		/*
 		 * We need to preallocate a transaction for a size update
@@ -1360,15 +1359,14 @@ xfs_vm_direct_IO(
 			ioend->io_isdirect = 1;
 		}
 
-		ret = __blockdev_direct_IO(rw, iocb, inode, bdev, iov,
+		ret = __blockdev_direct_IO(rw, iocb, inode, bdev, iter, offset,
 					    offset, nr_segs,
 					    xfs_get_blocks_direct,
 					    xfs_end_io_direct_write, NULL, 0);
 		if (ret != -EIOCBQUEUED && iocb->private)
 			goto out_trans_cancel;
 	} else {
-		ret = __blockdev_direct_IO(rw, iocb, inode, bdev, iov,
-					    offset, nr_segs,
+		ret = __blockdev_direct_IO(rw, iocb, inode, bdev, iter, offset,
 					    xfs_get_blocks_direct,
 					    NULL, NULL, 0);
 	}

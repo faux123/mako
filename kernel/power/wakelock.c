4c231b7767eb1f90ccfec5c1fe0d4fdfc5f4af59
faux123 <reioux@gmail.com>
2013-02-06 23:16:28 -0800

    wakelock: instead of directly going to suspend_mem, use the new suspend_freeze
    Signed-off-by: faux123 <reioux@gmail.com>

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -50,7 +50,7 @@ static struct workqueue_struct *suspend_sys_sync_work_queue;
 static DECLARE_COMPLETION(suspend_sys_sync_comp);
 struct workqueue_struct *suspend_work_queue;
 struct wake_lock main_wake_lock;
-suspend_state_t requested_suspend_state = PM_SUSPEND_MEM;
+suspend_state_t requested_suspend_state = PM_SUSPEND_FREEZE;
 static struct wake_lock unknown_wakeup;
 static struct wake_lock suspend_backoff_lock;
 

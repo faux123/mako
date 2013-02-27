475e985e1f48484355d66ccf93f50b28afb9d21b
faux123 <reioux@gmail.com>
2013-02-27 07:52:37 -0600

    speaker amp control: initial release for speaker amp control
    Signed-off-by: faux123 <reioux@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako-sound.c
+++ arch/arm/mach-msm/lge/mako/board-mako-sound.c
@@ -34,7 +34,10 @@
 #define AGC_COMPRESIION_RATE        0
 #define AGC_OUTPUT_LIMITER_DISABLE  1
 #define AGC_FIXED_GAIN              12
-
+#define AGC_ATK_TIME			5
+#define AGC_REL_TIME			11
+#define AGC_HOLD_TIME			0
+#define AGC_OUTPUT_LIMIT_LEVEL		26
 
 #define GPIO_EAR_MIC_BIAS_EN        PM8921_GPIO_PM_TO_SYS(20)
 #define GPIO_EAR_SENSE_N            82
@@ -99,6 +102,10 @@ static struct audio_amp_platform_data amp_platform_data =  {
 	.agc_compression_rate = AGC_COMPRESIION_RATE,
 	.agc_output_limiter_disable = AGC_OUTPUT_LIMITER_DISABLE,
 	.agc_fixed_gain = AGC_FIXED_GAIN,
+	.ATK_time = AGC_ATK_TIME,
+	.REL_time = AGC_REL_TIME,
+	.Hold_time = AGC_HOLD_TIME,
+	.Output_limit_level = AGC_OUTPUT_LIMIT_LEVEL,
 };
 #endif
 

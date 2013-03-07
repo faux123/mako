aa4a4072f63896f6511f78ad450a50807565285c
faux123 <reioux@gmail.com>
2013-03-06 18:51:19 -0600

    intellidemand: allow clean compilation when LMF is disabled
    bumped version to 4.2
    
    Signed-off-by: faux123 <reioux@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -299,6 +299,7 @@ extern bool lmf_screen_state;
 
 static void msm_cpu_early_suspend(struct early_suspend *h)
 {
+#ifdef CONFIG_CPUFREQ_LIMIT_MAX_FREQ
 	int cpu = 0;
 
 	for_each_possible_cpu(cpu) {
@@ -306,10 +307,12 @@ static void msm_cpu_early_suspend(struct early_suspend *h)
 		lmf_screen_state = false;
 		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
 	}
+#endif
 }
 
 static void msm_cpu_late_resume(struct early_suspend *h)
 {
+#ifdef CONFIG_CPUFREQ_LIMIT_MAX_FREQ
 	int cpu = 0;
 
 	for_each_possible_cpu(cpu) {
@@ -318,6 +321,7 @@ static void msm_cpu_late_resume(struct early_suspend *h)
 		lmf_screen_state = true;
 		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
 	}
+#endif
 }
 
 static struct early_suspend msm_cpu_early_suspend_handler = {

ecbcb41eef9188e5b86b24d7566dcb44bb4b5867
faux123 <reioux@gmail.com>
2013-03-14 02:16:09 -0500

    interactive: change permission for boostpulse from root to system
    for use with Android PowerHAL
    
    Signed-off-by: faux123 <reioux@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -34,6 +34,9 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
 
+#include <linux/syscalls.h>
+#include <linux/highuid.h>
+
 static int active_count;
 
 struct cpufreq_interactive_cpuinfo {
@@ -121,6 +124,18 @@ struct cpufreq_governor cpufreq_gov_interactive = {
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
 };
+
+#define	AID_SYSTEM	(1000)
+static void dbs_chown(void)
+{
+	int ret;
+
+	ret =
+	sys_chown("/sys/devices/system/cpu/cpufreq/interactive/boostpulse",
+		low2highuid(AID_SYSTEM), low2highgid(0));
+	if (ret)
+		pr_err("sys_chown: boostpulse error: %d", ret);
+}
 
 static void cpufreq_interactive_timer_resched(
 	struct cpufreq_interactive_cpuinfo *pcpu)
@@ -915,6 +930,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return -EINVAL;
 
 		mutex_lock(&gov_lock);
+
+		dbs_chown();
 
 		freq_table =
 			cpufreq_frequency_get_table(policy->cpu);

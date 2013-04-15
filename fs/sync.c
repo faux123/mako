4803cda1e81eaefb4d3cbd1d1174461e1fcc514f
Paul Reioux <reioux@gmail.com>
2013-04-15 14:30:06 -0500

    dynamic fsync: favor true case since most will be using this feature
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -170,7 +170,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (unlikely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -214,7 +214,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (unlikely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -224,7 +224,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (unlikely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -299,7 +299,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (unlikely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -403,7 +403,7 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (unlikely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif

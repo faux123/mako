61fc8fbe10279c1dd0af6c33714cdddadfbfad87
Paul Reioux <reioux@gmail.com>
2013-04-15 17:25:50 -0500

    Revert "mako: charger: disable hsic host and mdm driver in charger mode"
    This reverts commit a09c0cd9343cfc10a465c180512cbe90a71e9f7e.

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -2049,17 +2049,13 @@ static void __init apq8064_common_init(void)
 	platform_add_devices(common_devices, ARRAY_SIZE(common_devices));
 	platform_add_devices(common_not_mpq_devices,
 			ARRAY_SIZE(common_not_mpq_devices));
-	if (!mako_charger_mode) {
-		apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
-		device_initialize(&apq8064_device_hsic_host.dev);
-	}
+	apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
+	device_initialize(&apq8064_device_hsic_host.dev);
 	apq8064_pm8xxx_gpio_mpp_init();
 	apq8064_init_mmc();
 
-	if (!mako_charger_mode) {
-		mdm_8064_device.dev.platform_data = &mdm_platform_data;
-		platform_device_register(&mdm_8064_device);
-	}
+	mdm_8064_device.dev.platform_data = &mdm_platform_data;
+	platform_device_register(&mdm_8064_device);
 
 	platform_device_register(&apq8064_slim_ctrl);
 	slim_register_board_info(apq8064_slim_devices,

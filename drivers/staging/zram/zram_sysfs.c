939b66761a3b868e8c3f1553e1f7c040ea83fae8
Paul Reioux <reioux@gmail.com>
2013-06-09 23:44:52 -0500

    zram: optimize for Android use
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -68,7 +68,12 @@ static ssize_t disksize_store(struct device *dev,
 		pr_info("Cannot change disksize for initialized device\n");
 		return -EBUSY;
 	}
-
+#ifdef CONFIG_ZRAM_FOR_ANDROID
+        if (!disksize) {
+                disksize = default_disksize_perc_ram *
+                                        ((totalram_pages << PAGE_SHIFT) / 100);
+        }
+#endif
 	zram->disksize = PAGE_ALIGN(disksize);
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
 	up_write(&zram->init_lock);

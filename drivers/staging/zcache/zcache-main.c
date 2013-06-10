2173d9e4174adf8f9cc9decb895adcce3247c539
Paul Reioux <reioux@gmail.com>
2013-06-09 21:43:31 -0500

    zcache-main.c: fix compilation error due to API update
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -982,7 +982,7 @@ int zcache_new_client(uint16_t cli_id)
 		goto out;
 	cli->allocated = 1;
 #ifdef CONFIG_FRONTSWAP
-	cli->zspool = zs_create_pool("zcache", ZCACHE_GFP_MASK);
+	cli->zspool = zs_create_pool(ZCACHE_GFP_MASK);
 	if (cli->zspool == NULL)
 		goto out;
 	idr_init(&cli->tmem_pools);

412e29db80928fc965c8f65bb1dfa6f5affd05c0
Steve Kondik <shade@chemlab.org>
2013-08-03 23:12:06 -0700

    mako: Add support to query cpu status
    Change-Id: I34677c1f11b4f47027006e08b16e1450c3e31719

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1857,6 +1857,7 @@ static struct platform_device *cdp_devices[] __initdata = {
 #ifdef CONFIG_MSM_ROTATOR
        &msm_rotator_device,
 #endif
+	&msm8960_cpu_slp_status,
 };
 
 #ifdef CONFIG_USER_RC_INPUT

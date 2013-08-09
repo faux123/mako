dce4a756022b13adf0033c724b0ee0b8016b62de
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2013-08-09 13:07:32 -0500

    LZ4: compression/decompression signedness mismatch (v2)
    LZ4 compression and decompression functions require different
    in signedness input/output parameters: unsigned char for
    compression and signed char for decompression.
    
    Change decompression API to require "(const) unsigned char *".
    
    v2: minor coding style fix.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -64,8 +64,8 @@ int lz4hc_compress(const unsigned char *src, size_t src_len,
  *	note :  Destination buffer must be already allocated.
  *		a bit faster than lz4_decompress_unknownoutputsize()
  */
-int lz4_decompress(const char *src, size_t *src_len, char *dest,
-		size_t actual_dest_len);
+int lz4_decompress(const unsigned char *src, size_t *src_len,
+		unsigned char *dest, size_t actual_dest_len);
 
 /*
  * lz4_decompress_unknownoutputsize()
@@ -79,6 +79,6 @@ int lz4_decompress(const char *src, size_t *src_len, char *dest,
  *		  Error if return (< 0)
  *	note :  Destination buffer must be already allocated.
  */
-int lz4_decompress_unknownoutputsize(const char *src, size_t src_len,
-		char *dest, size_t *dest_len);
+int lz4_decompress_unknownoutputsize(const unsigned char *src, size_t src_len,
+		unsigned char *dest, size_t *dest_len);
 #endif

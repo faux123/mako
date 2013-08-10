cd15350bf4cd65e94d7c48ae76cc43e420495743
Paul Reioux <reioux@gmail.com>
2013-08-10 00:46:48 -0500

    lowmemorykiller: fix GCC-4.7 compiler warning
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -284,7 +284,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free;
-	int other_file;
+	int other_file = 0;
 	unsigned long nr_to_scan = sc->nr_to_scan;
 
 #ifdef CONFIG_ZRAM_FOR_ANDROID

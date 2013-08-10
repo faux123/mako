0957aeebe16dc05c3113774c2f26f207a6b52cf9
Paul Reioux <reioux@gmail.com>
2013-08-10 03:01:05 -0500

    zcache-main.c: use MACRO define instead of hardcoded default of lzo
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -43,6 +43,8 @@
 #include <linux/frontswap.h>
 #endif
 
+#define ZCACHE_COMPRESSOR_DEFAULT "lzo"
+
 #if 0
 /* this is more aggressive but may cause other problems? */
 #define ZCACHE_GFP_MASK	(GFP_ATOMIC | __GFP_NORETRY | __GFP_NOWARN)
@@ -1986,7 +1988,7 @@ static int __init zcache_comp_init(void)
 					zcache_comp_name);
 	}
 	if (!ret)
-		strcpy(zcache_comp_name, "lzo");
+		strcpy(zcache_comp_name, ZCACHE_COMPRESSOR_DEFAULT);
 	ret = crypto_has_comp(zcache_comp_name, 0, 0);
 	if (!ret) {
 		ret = 1;

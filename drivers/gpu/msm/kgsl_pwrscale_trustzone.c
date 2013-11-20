c78bce82bd40831d0c28b45aed94851dbe3f86d8
Paul Reioux <reioux@gmail.com>
2013-11-19 20:39:55 -0600

    KGSL: make simple govenor a selectable option, co-exist with ondemand
    also removed some hardcoded values
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/gpu/msm/kgsl_pwrscale_trustzone.c

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -26,6 +26,9 @@
 
 #define TZ_GOVERNOR_PERFORMANCE 0
 #define TZ_GOVERNOR_ONDEMAND    1
+#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
+#define TZ_GOVERNOR_SIMPLE	2
+#endif
 
 struct tz_priv {
 	int governor;
@@ -80,6 +83,10 @@ static ssize_t tz_governor_show(struct kgsl_device *device,
 
 	if (priv->governor == TZ_GOVERNOR_ONDEMAND)
 		ret = snprintf(buf, 10, "ondemand\n");
+#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
+	else if (priv->governor == TZ_GOVERNOR_SIMPLE)
+		ret = snprintf(buf, 8, "simple\n");
+#endif
 	else
 		ret = snprintf(buf, 13, "performance\n");
 
@@ -103,6 +110,10 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 
 	if (!strncmp(str, "ondemand", 8))
 		priv->governor = TZ_GOVERNOR_ONDEMAND;
+#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
+	else if (!strncmp(str, "simple", 6))
+		priv->governor = TZ_GOVERNOR_SIMPLE;
+#endif
 	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
@@ -132,7 +143,12 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv = pwrscale->priv;
 	if (device->state != KGSL_STATE_NAP &&
+#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
+		(priv->governor == TZ_GOVERNOR_ONDEMAND ||
+		 priv->governor == TZ_GOVERNOR_SIMPLE))
+#else
 		priv->governor == TZ_GOVERNOR_ONDEMAND)
+#endif
 		if (device->pwrctrl.constraint.type == KGSL_CONSTRAINT_NONE)
 			kgsl_pwrctrl_pwrlevel_change(device,
 					device->pwrctrl.default_pwrlevel);
@@ -142,6 +158,7 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 /* KGSL Simple GPU Governor */
 /* Copyright (c) 2011-2013, Paul Reioux (Faux123). All rights reserved. */
 static int lazyness = 5;
+static int ramp_up_threshold = 6000;
 
 static int simple_governor(struct kgsl_device *device, int idle_stat)
 {
@@ -149,7 +166,7 @@ static int simple_governor(struct kgsl_device *device, int idle_stat)
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 
 	/* it's currently busy */
-	if (idle_stat < 6000) {
+	if (idle_stat < ramp_up_threshold) {
 		if (pwr->active_pwrlevel == 0)
 			val = 0; /* already maxed, so do nothing */
 		else if ((pwr->active_pwrlevel > 0) &&
@@ -208,16 +225,27 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		idle = priv->bin.total_time - priv->bin.busy_time;
 		idle = (idle > 0) ? idle : 0;
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-		val = simple_governor(device, idle);
+		if (priv->governor == TZ_GOVERNOR_SIMPLE)
+			val = simple_governor(device, idle);
+		else
+			val = __secure_tz_entry2(TZ_UPDATE_ID, idle,
+				device->id);
 #else
 		val = __secure_tz_entry2(TZ_UPDATE_ID, idle, device->id);
 #endif
 	} else {
 		if (pwr->step_mul > 1)
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-			idle = priv->bin.total_time - priv->bin.busy_time;
-			idle = (idle > 0) ? idle : 0;
-			val = simple_governor(device, idle);
+			if (priv->governor == TZ_GOVERNOR_SIMPLE) {
+				idle = priv->bin.total_time -
+					priv->bin.busy_time;
+				idle = (idle > 0) ? idle : 0;
+				val = simple_governor(device, idle);
+			} else
+				val = __secure_tz_entry3(TZ_UPDATE_ID,
+					(pwr->active_pwrlevel + 1)/2,
+					priv->bin.total_time,
+					priv->bin.busy_time);
 #else
 			val = __secure_tz_entry3(TZ_UPDATE_ID,
 				(pwr->active_pwrlevel + 1)/2,
@@ -225,9 +253,17 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 #endif
 		else
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-			idle = priv->bin.total_time - priv->bin.busy_time;
-			idle = (idle > 0) ? idle : 0;
-			val = simple_governor(device, idle);
+			if (priv->governor == TZ_GOVERNOR_SIMPLE) {
+				idle = priv->bin.total_time -
+					priv->bin.busy_time;
+				idle = (idle > 0) ? idle : 0;
+				val = simple_governor(device, idle);
+			} else
+				val = __secure_tz_entry3(TZ_UPDATE_ID,
+					pwr->active_pwrlevel,
+					priv->bin.total_time,
+					priv->bin.busy_time);
+
 #else
 			val = __secure_tz_entry3(TZ_UPDATE_ID,
 				pwr->active_pwrlevel,

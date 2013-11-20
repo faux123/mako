0a6937d011577530f0ef071e7911d561795a3cb4
Paul Reioux <reioux@gmail.com>
2013-11-19 20:41:46 -0600

    LCD Gamma Hack: sync with CM10.2 merged updates
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -38,6 +38,7 @@ static struct dsi_buf lgit_tx_buf;
 static struct dsi_buf lgit_rx_buf;
 static struct msm_fb_data_type *local_mfd;
 static int skip_init;
+static int lcd_isactive = 0;
 
 #define DSV_ONBST 57
 
@@ -84,6 +85,7 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
+	lcd_isactive = 1;
 	MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 	ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
 //			mipi_lgit_pdata->power_on_set_1,
@@ -146,6 +148,7 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
+	lcd_isactive = 0;
 	MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 	ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
 			mipi_lgit_pdata->power_off_set_1,
@@ -372,13 +375,36 @@ static ssize_t kgamma_b_show(struct device *dev, struct device_attribute *attr,
 		kgamma[8], kgamma[9]);
 }
 
-static ssize_t kgamma_ctrl_store(struct device *dev,
+static ssize_t kgamma_apply_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
+	int ret = 0;
+
+	/*
+	 * Only attempt to apply if the LCD is active.
+	 * If it isn't, the device will panic-reboot
+	 */
+	if(lcd_isactive) {
+		MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
+		ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
+		faux123_power_on_set_1,
+		mipi_lgit_pdata->power_on_set_size_1);
+		MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x14000000);
+		if (ret < 0) {
+			pr_err("%s: failed to transmit power_on_set_1 cmds\n",
+				__func__);
+			return ret;
+		}
+	} else {
+		pr_err("%s: Tried to apply gamma settings when LCD was off\n",
+			__func__);
+		//Is ENODEV correct here?  Perhaps it should be something else?
+		return -ENODEV;
+	}
 	return count;
 }
 
-static ssize_t kgamma_ctrl_show(struct device *dev,
+static ssize_t kgamma_apply_show(struct device *dev,
 				struct device_attribute *attr, char *buf)
 {
 	return 0;
@@ -387,7 +413,7 @@ static ssize_t kgamma_ctrl_show(struct device *dev,
 static DEVICE_ATTR(kgamma_r, 0644, kgamma_r_show, kgamma_r_store);
 static DEVICE_ATTR(kgamma_g, 0644, kgamma_g_show, kgamma_g_store);
 static DEVICE_ATTR(kgamma_b, 0644, kgamma_b_show, kgamma_b_store);
-static DEVICE_ATTR(kgamma_ctrl, 0644, kgamma_ctrl_show, kgamma_ctrl_store);
+static DEVICE_ATTR(kgamma_apply, 0644, kgamma_apply_show, kgamma_apply_store);
 
 /******************* end sysfs interface *******************/
 
@@ -401,7 +427,8 @@ static int mipi_lgit_lcd_probe(struct platform_device *pdev)
 	}
 
 	// make a copy of platform data
-	memcpy((void*)faux123_power_on_set_1, (void*)mipi_lgit_pdata->power_on_set_1, 
+	memcpy((void*)faux123_power_on_set_1,
+		(void*)mipi_lgit_pdata->power_on_set_1, 
 		sizeof(faux123_power_on_set_1));
 
 	pr_info("%s start\n", __func__);
@@ -419,7 +446,7 @@ static int mipi_lgit_lcd_probe(struct platform_device *pdev)
 	rc = device_create_file(&pdev->dev, &dev_attr_kgamma_b);
 	if(rc !=0)
 		return -1;
-	rc = device_create_file(&pdev->dev, &dev_attr_kgamma_ctrl);
+	rc = device_create_file(&pdev->dev, &dev_attr_kgamma_apply);
 	if(rc !=0)
 		return -1;
 

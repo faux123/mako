57147a5443bc1b1e8e918df475e971e914f4480b
Paul Reioux <reioux@gmail.com>
2013-11-20 01:07:50 -0600

    Sound Control: (OPTIONAL) add power amp registers access control as well
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -37,6 +37,10 @@ int reg_access(unsigned int reg)
 	int ret = 1;
 
 	switch (reg) {
+		case TABLA_A_RX_HPH_L_GAIN:
+		case TABLA_A_RX_HPH_R_GAIN:
+		case TABLA_A_RX_HPH_L_STATUS:
+		case TABLA_A_RX_HPH_R_STATUS:
 		case TABLA_A_CDC_RX1_VOL_CTL_B2_CTL:
 		case TABLA_A_CDC_RX2_VOL_CTL_B2_CTL:
 		case TABLA_A_CDC_RX3_VOL_CTL_B2_CTL:

c29434a997c70baa6eeaf5542390f7759378075b
Paul Reioux <reioux@gmail.com>
2014-03-13 01:02:52 -0700

    intelli_plug: switch to use dedicated high priority workqueue
    from the shared global workqueue.  This should prevent hang ups while the
    global workqueue is busy and cleaned up some logic issues.
    
    give input boost its own workqueue
    
    bump to version 2.2
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -1,7 +1,7 @@
 /*
  * Author: Paul Reioux aka Faux123 <reioux@gmail.com>
  *
- * Copyright 2012 Paul Reioux
+ * Copyright 2012~2014 Paul Reioux
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -30,7 +30,7 @@
 #undef DEBUG_INTELLI_PLUG
 
 #define INTELLI_PLUG_MAJOR_VERSION	2
-#define INTELLI_PLUG_MINOR_VERSION	0
+#define INTELLI_PLUG_MINOR_VERSION	2
 
 #define DEF_SAMPLING_MS			(1000)
 #define BUSY_SAMPLING_MS		(500)
@@ -43,11 +43,15 @@
 
 #define RUN_QUEUE_THRESHOLD		38
 
-#define CPU_DOWN_FACTOR			3
+#define CPU_DOWN_FACTOR			2
 
 static DEFINE_MUTEX(intelli_plug_mutex);
 
-struct delayed_work intelli_plug_work;
+static struct delayed_work intelli_plug_work;
+static struct delayed_work intelli_plug_boost;
+
+static struct workqueue_struct *intelliplug_wq;
+static struct workqueue_struct *intelliplug_boost_wq;
 
 static unsigned int intelli_plug_active = 0;
 module_param(intelli_plug_active, uint, 0644);
@@ -110,7 +114,8 @@ static int mp_decision(void)
 
 	if (nr_cpu_online) {
 		index = (nr_cpu_online - 1) * 2;
-		if ((nr_cpu_online < 4) && (rq_depth >= NwNs_Threshold[index])) {
+		if ((nr_cpu_online < 4) &&
+			(rq_depth >= NwNs_Threshold[index])) {
 			if (total_time >= TwTs_Threshold[index]) {
 				new_state = 1;
 			}
@@ -168,6 +173,15 @@ static unsigned int calculate_thread_stats(void)
 	nr_run_last = nr_run;
 
 	return nr_run;
+}
+
+static void __cpuinit intelli_plug_boost_fn(struct work_struct *work)
+{
+
+	int nr_cpus = num_online_cpus();
+
+	if (nr_cpus < 2)
+		cpu_up(1);
 }
 
 static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
@@ -285,7 +299,7 @@ static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
 			pr_info("intelli_plug is suspened!\n");
 #endif
 	}
-	schedule_delayed_work_on(0, &intelli_plug_work,
+	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
 		msecs_to_jiffies(sampling_time));
 }
 
@@ -293,9 +307,9 @@ static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
 static void intelli_plug_suspend(struct power_suspend *handler)
 {
 	int i;
-	int num_of_active_cores = 4;
+	int num_of_active_cores = num_possible_cpus();
 	
-	cancel_delayed_work_sync(&intelli_plug_work);
+	flush_workqueue(intelliplug_wq);
 
 	mutex_lock(&intelli_plug_mutex);
 	suspended = true;
@@ -314,7 +328,7 @@ static void __cpuinit intelli_plug_resume(struct power_suspend *handler)
 
 	mutex_lock(&intelli_plug_mutex);
 	/* keep cores awake long enough for faster wake up */
-	persist_count = DUAL_CORE_PERSISTENCE;
+	persist_count = BUSY_PERSISTENCE;
 	suspended = false;
 	mutex_unlock(&intelli_plug_mutex);
 
@@ -322,13 +336,13 @@ static void __cpuinit intelli_plug_resume(struct power_suspend *handler)
 	if (eco_mode_active)
 		num_of_active_cores = 2;
 	else
-		num_of_active_cores = 4;
+		num_of_active_cores = num_possible_cpus();
 
 	for (i = 1; i < num_of_active_cores; i++) {
 		cpu_up(i);
 	}
 
-	schedule_delayed_work_on(0, &intelli_plug_work,
+	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
 		msecs_to_jiffies(10));
 }
 
@@ -344,14 +358,8 @@ static void intelli_plug_input_event(struct input_handle *handle,
 #ifdef DEBUG_INTELLI_PLUG
 	pr_info("intelli_plug touched!\n");
 #endif
-
-	cancel_delayed_work(&intelli_plug_work);
-
-	sampling_time = BUSY_SAMPLING_MS;
-	busy_persist_count = BUSY_PERSISTENCE;
-
-        schedule_delayed_work_on(0, &intelli_plug_work,
-                msecs_to_jiffies(sampling_time));
+	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_boost,
+		msecs_to_jiffies(10));
 }
 
 static int input_dev_filter(const char *input_dev_name)
@@ -432,16 +440,19 @@ int __init intelli_plug_init(void)
 		 INTELLI_PLUG_MAJOR_VERSION,
 		 INTELLI_PLUG_MINOR_VERSION);
 
-	sampling_time = DEF_SAMPLING_MS;
-
 	rc = input_register_handler(&intelli_plug_input_handler);
 #ifdef CONFIG_POWERSUSPEND
 	register_power_suspend(&intelli_plug_power_suspend_driver);
 #endif
 
+	intelliplug_wq = alloc_workqueue("intelliplug",
+				WQ_HIGHPRI | WQ_UNBOUND, 1);
+	intelliplug_boost_wq = alloc_workqueue("iplug_boost",
+				WQ_HIGHPRI | WQ_UNBOUND, 1);
 	INIT_DELAYED_WORK(&intelli_plug_work, intelli_plug_work_fn);
-	schedule_delayed_work_on(0, &intelli_plug_work,
-		msecs_to_jiffies(sampling_time));
+	INIT_DELAYED_WORK(&intelli_plug_boost, intelli_plug_boost_fn);
+	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
+		msecs_to_jiffies(10));
 
 	return 0;
 }

61d14c53778600f39ba3da5d1e64e87b1434c555
Paul Reioux <reioux@gmail.com>
2014-03-22 20:30:50 -0700

    block/bfq-iosched.c: GCC 4.8.x compatibility fixup
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -2806,7 +2806,7 @@ static ssize_t								\
 __FUNC(struct elevator_queue *e, const char *page, size_t count)	\
 {									\
 	struct bfq_data *bfqd = e->elevator_data;			\
-	unsigned long __data;						\
+	unsigned long __data = 0;						\
 	int ret = bfq_var_store(&__data, (page), count);		\
 	if (__data < (MIN))						\
 		__data = (MIN);						\
@@ -2866,7 +2866,7 @@ static ssize_t bfq_max_budget_store(struct elevator_queue *e,
 				    const char *page, size_t count)
 {
 	struct bfq_data *bfqd = e->elevator_data;
-	unsigned long __data;
+	unsigned long __data = 0;
 	int ret = bfq_var_store(&__data, (page), count);
 
 	if (__data == 0)
@@ -2886,7 +2886,7 @@ static ssize_t bfq_timeout_sync_store(struct elevator_queue *e,
 				      const char *page, size_t count)
 {
 	struct bfq_data *bfqd = e->elevator_data;
-	unsigned long __data;
+	unsigned long __data = 0;
 	int ret = bfq_var_store(&__data, (page), count);
 
 	if (__data < 1)
@@ -2905,7 +2905,7 @@ static ssize_t bfq_low_latency_store(struct elevator_queue *e,
 				     const char *page, size_t count)
 {
 	struct bfq_data *bfqd = e->elevator_data;
-	unsigned long __data;
+	unsigned long __data = 0;
 	int ret = bfq_var_store(&__data, (page), count);
 
 	if (__data > 1)

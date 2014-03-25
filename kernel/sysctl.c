f39dadf91b5dd8c867a6a2a60ffe10bb6e29251f
Paul Reioux <reioux@gmail.com>
2014-03-24 21:57:01 -0700

    page_alloc: Make watermarks tunable separately
    This patch introduces three new sysctls to /proc/sys/vm:
    wmark_min_kbytes, wmark_low_kbytes and wmark_high_kbytes.
    
    Each entry is used to compute watermark[min], watermark[low]
    and watermark[high] for each zone.
    
    These parameters are also updated when min_free_kbytes are
    changed because originally they are set based on min_free_kbytes.
    On the other hand, min_free_kbytes is updated when wmark_free_kbytes
    changes.
    
    By using the parameters one can adjust the difference among
    watermark[min], watermark[low] and watermark[high] and as a result
    one can tune the kernel reclaim behaviour to fit their requirement.
    
    Signed-off-by: Satoru Moriya <satoru.moriya@hds.com>
    
    modified and tuned for Hammerhead
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -102,6 +102,7 @@ extern char core_pattern[];
 extern unsigned int core_pipe_limit;
 extern int pid_max;
 extern int min_free_kbytes;
+extern int wmark_min_kbytes, wmark_low_kbytes, wmark_high_kbytes;
 extern int min_free_order_shift;
 extern int pid_max_min, pid_max_max;
 extern int sysctl_drop_caches;
@@ -1011,6 +1012,32 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 },
 #endif
+	{
+		.procname	= "wmark_min_kbytes",
+		.data		= &wmark_min_kbytes,
+		.maxlen		= sizeof(wmark_min_kbytes),
+		.mode		= 0644,
+		.proc_handler	= wmark_min_kbytes_sysctl_handler,
+		.extra1		= &zero,
+		.extra2		= &wmark_low_kbytes,
+	},
+	{
+		.procname	= "wmark_low_kbytes",
+		.data		= &wmark_low_kbytes,
+		.maxlen		= sizeof(wmark_low_kbytes),
+		.mode		= 0644,
+		.proc_handler	= wmark_low_kbytes_sysctl_handler,
+		.extra1		= &wmark_min_kbytes,
+		.extra2		= &wmark_high_kbytes,
+	},
+	{
+		.procname	= "wmark_high_kbytes",
+		.data		= &wmark_high_kbytes,
+		.maxlen		= sizeof(wmark_high_kbytes),
+		.mode		= 0644,
+		.proc_handler	= wmark_high_kbytes_sysctl_handler,
+		.extra1		= &wmark_low_kbytes,
+	},
 /*
  * NOTE: do not add new entries to this table unless you have read
  * Documentation/sysctl/ctl_unnumbered.txt
